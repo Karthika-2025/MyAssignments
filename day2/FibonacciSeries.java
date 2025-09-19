1b2f2d3110cafc56d3360ace0c51fcbc5b77296d
Karthika-2025 <karthika19.it@gmail.com>
2025-09-19 19:26:24 +0530

    Add files via upload

---
+++ day2/FibonacciSeries.java
@@ -0,0 +1,20 @@
+package week1.day2;
+
+import java.util.Iterator;
+
+public class FibonacciSeries 
+{
+
+	public static void main(String[] args)
+	{
+		int fibo=0;
+		for(int i=0;i<=8;i++)
+		{
+			System.out.println(i);
+			i=fibo+i;
+
+		}
+
+	}
+
+}
