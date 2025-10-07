7c8e955b3512e53a39358771f5bea12490a2baa5
Karthika-2025 <karthika19.it@gmail.com>
2025-10-07 19:10:45 +0530

    Add files via upload

---
+++ day2/HomeAssignment/IsPrime.java
@@ -0,0 +1,27 @@
+package week1.day2.HomeAssignment;
+
+public class IsPrime {
+
+	public static void main(String[] args)
+	{
+		int n= 1;
+		boolean isprime=true;
+		System.out.println("The given number is "+n);
+		for(int i=2;i<=n-1;i++)
+		{
+			if(n%i == 0)
+			{
+				isprime=false;
+				break;
+			}			
+		}
+		if(isprime==true)
+		{
+			System.out.println(n+" is prime number");	
+		}
+		else
+		{
+			System.out.println(n+" is not prime number");	
+		}	
+	}
+}
