3bed354a9bf73f1c57cb3c4bfd283f8df396f340
Karthika-2025 <karthika19.it@gmail.com>
2025-10-11 19:55:58 +0530

    Add files via upload

--- week2/day2/HomeAssignment/facebookAccountCReation.java
+++ week2/day2/HomeAssignment/facebookAccountCReation.java
@@ -1,5 +1,6 @@
 package week2.day2.HomeAssignment;
 
+import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -7,20 +8,24 @@ import org.openqa.selenium.support.ui.Select;
 
 public class facebookAccountCReation 
 {
-
 	public static void main(String[] args) throws InterruptedException 
 	{
+		//Driver initialization
 		ChromeDriver driver=new ChromeDriver();
+		
+		//Open Facebook & maximize the window
 		driver.get("https://en-gb.facebook.com/");
 		driver.manage().window().maximize();
 		Thread.sleep(5000);
 		
+		//Click CREATE NEW ACCOUNT button
 		driver.findElement(By.linkText("Create new account")).click();
+		
+		//FIRST NAME & LAST NAME text box filled
 		driver.findElement(By.name("firstname")).sendKeys("Krithikesh");		
 		driver.findElement(By.name("lastname")).sendKeys("Krithikesh");
-		driver.findElement(By.id("u_0_h_0c")).sendKeys("1234567891");
-		driver.findElement(By.id("password_step_input")).sendKeys("abcabc");
 		
+		//DOB drop down filled
 		WebElement birthDay= driver.findElement(By.name("birthday_day"));
 		Select DayOption=new Select(birthDay);
 		DayOption.selectByValue("14");
@@ -33,19 +38,17 @@ public class facebookAccountCReation
 		Select BirthYear =new Select(birthYear);
 		BirthYear.selectByVisibleText("1991");
 		
+		//GENDER radio button selected using XPATH
+		driver.findElement(By.xpath("//input[@value='1']")).click();
 		
-		
-		
-		
-
-		
-		
-
-
-		)
-		
-		
-
+		//EMAIL & PASSWORD filled
+		driver.findElement(By.name("reg_email__")).sendKeys("1234567891");
+		driver.findElement(By.id("password_step_input")).sendKeys("abcabc");
+			
+		//IMPLICIT WAIT used
+		driver.manage().timeouts().implicitlyWait(Duration.ofMinutes(1));
+		System.out.println(driver.getTitle());
+		driver.close();
 	}
 
 }
